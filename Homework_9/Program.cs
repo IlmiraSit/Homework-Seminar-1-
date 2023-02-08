9dd1b7946a73a03a1c7ae877824eac78b9135d12
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-08 22:17:42 +0300

    home9

---
+++ Homework_9/Program.cs
@@ -0,0 +1,8 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int digitsCount = (int)Math.Log10(number)+1;
+int multiplier = (int)Math.Pow(10, digitsCount - 2);
+int result = number % multiplier + number / multiplier / 10 * multiplier;
+Console.WriteLine(result);
+
