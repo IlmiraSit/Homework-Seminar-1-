217745a36cb075885d17d0668ff102b03c849de9
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-08 22:20:04 +0300

    home 10

---
+++ Homework_10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+if (a % b == 0)
+{
+    Console.WriteLine("Kratno");
+}
+else if (b % a == 0)
+{
+    Console.WriteLine("Kratno");
+}
+else
+{
+    Console.WriteLine("Not");
+}
+
