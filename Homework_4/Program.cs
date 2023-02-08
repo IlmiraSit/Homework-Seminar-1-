74a885d883aa15c0218ceab770454125eb0f0181
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-08 22:03:24 +0300

    home4

---
+++ Homework_4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
+
