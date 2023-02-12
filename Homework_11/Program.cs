e553d07c23e9310f05e427e93c04ad7db1b77155
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 13:15:33 +0300

    3 number

---
+++ Homework_11/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Enter a number: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+while(a >= 999)
+a = a / 10;
+Console.WriteLine($"Third digit: {a % 10}");
+if (a <= 999);
+ Console.WriteLine("there is no third number");
